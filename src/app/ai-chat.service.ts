3c000a9b13955ea00da7ce2b714c5c9a8495d937
Sridhar Uyyala <sridhar.uyyala@gmail.com>
2019-03-17 20:34:00 -0500

    Post messages to Room

--- src/app/ai-chat.service.ts
+++ src/app/ai-chat.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {AngularFirestore} from '@angular/fire/firestore';
 import {map} from 'rxjs/operators';
+import {firestore} from 'firebase';
 
 @Injectable({
   providedIn: 'root'
@@ -14,9 +15,23 @@ export class AiChatService {
   constructor(private readonly afs: AngularFirestore) {
   }
 
-  postMessage(message: any) {
-    console.log('In PostMessage', message);
-    this.chatRoom.update(message);
+  postMessage(msg: any) {
+    console.log('In PostMessage', msg);
+    const chatRoomRef = this.afs.collection('ai-chats')
+      .doc('room1')
+      .collection('docs')
+      .doc('messages');
+    chatRoomRef.update({
+      messages: firestore.FieldValue.arrayUnion({
+        message: msg,
+        userId: 'rahim'
+      })
+    }).then( () => {
+      console.log('msg posted successfully');
+    }).catch( (error) => {
+      console.log('failed to post msg', error);
+    });
+
   }
 
   getMessage() {
